ecc8de1f5f2f81d5926954dbdb0f3e7b8a2741d3
solevic <victor.solevic@epitech.eu>
2015-02-17 19:29:35 -0800

    Project initialization

---
+++ app/src/main/java/edu/csulb/android/fullcount/HomeActivity.java
@@ -0,0 +1,16 @@
+package edu.csulb.android.fullcount;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Victor Solevic on 17/02/2015.
+ */
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+}
---
+++ app/src/main/java/edu/csulb/android/fullcount/PlayerCardActivity.java
@@ -0,0 +1,14 @@
+package edu.csulb.android.fullcount;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayerCardActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_player_card);
+	}
+
+}
