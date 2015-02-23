50aeba48b2ff4b641c562f29733502c530260370
Sage56 <sage_h56@hotmail.com>
2015-02-22 22:08:13 -0800

    Log In Page
    Did as much as I could, but couldn't figure out how to piece it all
    together

--- app/src/main/java/edu/csulb/android/fullcount/LoginActivity.java
+++ app/src/main/java/edu/csulb/android/fullcount/LoginActivity.java
@@ -2,13 +2,92 @@ package edu.csulb.android.fullcount;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends Activity {
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-	@Override
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    private static final String Query_URL = "http://fullcount.azurewebsites.net";
+	//@Override
+    //TextView loginTextView;
+    Button loginButton;
+    EditText loginUsername;
+    EditText loginPassword;
+    JSONObject jsonobj;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+        //loginTextView = (TextView) findViewById(R.id.login_textview);
+        //loginTextView.setText("Set in Java");
+        loginUsername = (EditText) findViewById(R.id.login_username);
+        loginPassword = (EditText) findViewById(R.id.login_password);
+        loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(this);
+        try{
+            jsonobj = new JSONObject();
+            jsonobj.put("Username", loginUsername);
+            jsonobj.put("Password", loginPassword);
+            postData(jsonobj, "/login");
+        } catch (JSONException je){
+
+        }
 	}
 
+    @Override
+    //This represents what will happen when the button is pushed
+    public void onClick(View v) {
+//postData(loginUsername, );
+    }
+
+    new Thread(new Runnable() {
+        public void run() {
+            try {
+                JSONObject jsonobj = new JSONObject();
+                jsonobj.put("username", "bob");
+                jsonobj.put("password", "bob");
+                postData(jsonobj, "/login");
+            } catch (JSONException je) {
+            }
+        }).start();
+
+    public void postData(JSONObject data, String url) {
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost("http://fullcount.azurewebsites.net" + url);
+        try {
+            StringEntity dataStringEntity = new StringEntity(data.toString());
+            dataStringEntity.setContentType("application/json;charset=UTF-8");
+            dataStringEntity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json;charset=UTF-8"));
+            post.setEntity(dataStringEntity);
+
+            //Execute HTTP Post Request
+            HttpResponse response = client.execute(post);
+            Log.e("test", EntityUtils.toString(response.getEntity()));
+            Log.d("test", "NEXT");
+        } catch (ClientProtocolException e) {
+            //TODO Auto-generated catch block
+        } catch (IOException e) {
+            //TODO Auto-generated catch block
+        }
+    }
 }
