be9870fd31b9f61784f7eb6d6c57bc07bd4d19fb
llacap <lawrencelacap@gmail.com>
2015-03-14 16:07:37 -0700

    Team Roster Update 3/14

--- app/src/main/java/edu/csulb/android/fullcount/TeamRoster.java
+++ app/src/main/java/edu/csulb/android/fullcount/TeamRoster.java
@@ -1,5 +1,6 @@
 package edu.csulb.android.fullcount;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,18 +16,54 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.app.Activity;
 
-public class TeamRoster extends ActionBarActivity {
+public class TeamRoster extends Activity {
+
+    private HttpHelper httpHelp = new HttpHelper();
+    private Button AddPlayerButton, DoneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_roster);
 
-    }
+        AddPlayerButton = (Button)findViewById(R.id.AddPlayerButton);
+        AddPlayerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Go Back to login page
+                Toast.makeText(getBaseContext(),"Player Fragment Opened",Toast.LENGTH_SHORT).show();
+                //Intent i = new Intent(getApplicationContext(), FragmentAddPlayer.class);
+                //startActivity(i);
 
+            }});
+
+        String[] players = new String[] {"1", "2", "3"};
+
+
+        Integer[] imgid = new Integer[players.length];
+        for (int i = 0; i < players.length; i++) {
+            imgid[i] = R.drawable.avatar_icon;
+        }
+
+        CustomListAdapter cLA = new CustomListAdapter(this, players, imgid);
+        ListView listView = (ListView) findViewById(R.id.team_roster_list);
+        listView.setAdapter(cLA);
+
+
+        DoneButton = (Button)findViewById(R.id.DoneButton);
+        DoneButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Go Back to login page
+                //Toast.makeText(getBaseContext(),"Back to team page",Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), TeamCreation.class);
+                startActivity(i);
+            }});
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
