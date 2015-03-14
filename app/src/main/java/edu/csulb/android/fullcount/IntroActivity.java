8c38f6fa2713d941bf4efae82fdcd1649d8f3032
solevic <victor.solevic@epitech.eu>
2015-03-14 15:28:34 -0700

    Added Facebook login without autologin

--- app/src/main/java/edu/csulb/android/fullcount/IntroActivity.java
+++ app/src/main/java/edu/csulb/android/fullcount/IntroActivity.java
@@ -1,37 +1,54 @@
 package edu.csulb.android.fullcount;
 
-import android.content.Context;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
 
-public class IntroActivity extends FragmentActivity {
-    //private FacebookFragment facebookFragment;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class IntroActivity extends Activity {
+
+	private HttpHelper mHttpHelp = new HttpHelper();
+
+	private ProgressDialog mProgress;
+	private LoginButton mFacebookConnect;
+	private UiLifecycleHelper mUiHelper;
+
+	private Session.StatusCallback mCallback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state, Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+	};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        /*if (savedInstanceState == null) {
-            // Add the fragment on initial activity setup
-            facebookFragment = new FacebookFragment();
+	    mUiHelper = new UiLifecycleHelper(this, mCallback);
+	    mUiHelper.onCreate(savedInstanceState);
 
-            getSupportFragmentManager().beginTransaction()
-                    .add(android.R.id.content, facebookFragment).commit();
-        } else {
-            // Or set the fragment from restored state info
-            facebookFragment = (FacebookFragment) getSupportFragmentManager()
-                    .findFragmentById(android.R.id.content);
-        }*/
+	    mFacebookConnect = (LoginButton) findViewById(R.id.authButton);
+	    mFacebookConnect.setReadPermissions(Arrays.asList("user_location", "email"));
 
         Button logInBtn = (Button)findViewById(R.id.login_btn);
         logInBtn.setOnClickListener(new View.OnClickListener() {
@@ -52,6 +69,93 @@ public class IntroActivity extends FragmentActivity {
         });
     }
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mUiHelper.onResume();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		mUiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mUiHelper.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mUiHelper.onDestroy();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mUiHelper.onSaveInstanceState(outState);
+	}
+
+	protected void startProgressDialog() {
+		startProgressDialog(null, "Loading...");
+	}
+
+	protected void startProgressDialog(String title, String message) {
+		mProgress = ProgressDialog.show(this, title, message);
+	}
+
+	protected void dismissProgressDialog() {
+		if (mProgress != null) {
+
+			if (mProgress.isShowing()) {
+				mProgress.dismiss();
+			}
+
+			mProgress = null;
+		}
+	}
+
+	private void connectFacebook(String accessToken) {
+		startProgressDialog();
+
+		JSONObject jsonobj = new JSONObject();
+		try {
+			jsonobj.put("token", accessToken);
+		} catch (JSONException je) {
+		}
+
+		//sends the json object to the server via our HttpHelper activity
+		HttpResponse response = mHttpHelp.post("/api/users/login/facebook", jsonobj, "", "");
+
+		dismissProgressDialog();
+
+		if (response != null && response.getStatusLine().getStatusCode() == 200) {
+			Intent i = new Intent(getBaseContext(), HomeActivity.class);
+			startActivity(i);
+		} else {
+			Session.getActiveSession().closeAndClearTokenInformation();
+
+			try {
+				Toast.makeText(this, EntityUtils.toString(response.getEntity()), Toast.LENGTH_SHORT).show();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		mFacebookConnect.setEnabled(true);
+	}
+
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+		if (state.isOpened()) {
+			mFacebookConnect.setEnabled(false);
+			connectFacebook(session.getAccessToken());
+		} else if (state.isClosed()) {
+			mFacebookConnect.setEnabled(true);
+		}
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
